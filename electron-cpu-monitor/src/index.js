9fdc877c897157538bd7d8b35771187a678ad82e
val8119 <48407846+val8119@users.noreply.github.com>
2021-01-31 16:01:49 +0200

    *

--- electron-cpu-monitor/src/index.js
+++ electron-cpu-monitor/src/index.js
@@ -1,45 +1,60 @@
-const { app, BrowserWindow } = require('electron');
-const path = require('path');
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+const os = require("os-utils");
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
-  app.quit();
+if (require("electron-squirrel-startup")) {
+   // eslint-disable-line global-require
+   app.quit();
 }
 
 const createWindow = () => {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-  });
+   // Create the browser window.
+   const mainWindow = new BrowserWindow({
+      width: 450,
+      height: 300,
+      icon: __dirname + "/favicon.ico",
+      webPreferences: {
+         nodeIntegration: true,
+      },
+   });
 
-  // and load the index.html of the app.
-  mainWindow.loadFile(path.join(__dirname, 'index.html'));
+   // and load the index.html of the app.
+   mainWindow.loadFile(path.join(__dirname, "index.html"));
 
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+   mainWindow.setResizable(false);
+
+   mainWindow.removeMenu();
+
+   setInterval(function () {
+      os.cpuUsage(function (v) {
+         mainWindow.webContents.send("cpuUsage", v * 100);
+         mainWindow.webContents.send("freeMemory", os.freememPercentage() * 100);
+         mainWindow.webContents.send("totalMemory", os.totalmem() / 1024);
+      });
+   }, 1000);
 };
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+app.on("ready", createWindow);
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+app.on("window-all-closed", () => {
+   if (process.platform !== "darwin") {
+      app.quit();
+   }
 });
 
-app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
-  }
+app.on("activate", () => {
+   // On OS X it's common to re-create a window in the app when the
+   // dock icon is clicked and there are no other windows open.
+   if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+   }
 });
 
 // In this file you can include the rest of your app's specific main process
